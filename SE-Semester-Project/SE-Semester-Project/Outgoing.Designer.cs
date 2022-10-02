1bf6f6c3334a1b161dbfbc6c935aadd97daee566
Tyler Still <tstill2@uco.edu>
2022-10-02 10:48:14 -0500

    Changed layout for outgoing screen. Added file browsing and file name saving.

--- SE-Semester-Project/SE-Semester-Project/Outgoing.Designer.cs
+++ SE-Semester-Project/SE-Semester-Project/Outgoing.Designer.cs
@@ -29,24 +29,20 @@
         private void InitializeComponent()
         {
             this.btnSend = new System.Windows.Forms.Button();
-            this.btnAudio = new System.Windows.Forms.Button();
-            this.btnClear = new System.Windows.Forms.Button();
-            this.btnMessages = new System.Windows.Forms.Button();
             this.cboAddresses = new System.Windows.Forms.ComboBox();
-            this.grpMain = new System.Windows.Forms.GroupBox();
-            this.btnVideo = new System.Windows.Forms.Button();
-            this.grpMedia = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.txtOutgoing = new System.Windows.Forms.RichTextBox();
-            this.grpMain.SuspendLayout();
-            this.grpMedia.SuspendLayout();
+            this.cboFileList = new System.Windows.Forms.ComboBox();
+            this.btnCompose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSend
             // 
-            this.btnSend.BackColor = System.Drawing.Color.LightBlue;
+            this.btnSend.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnSend.Enabled = false;
             this.btnSend.ForeColor = System.Drawing.Color.Black;
-            this.btnSend.Location = new System.Drawing.Point(216, -1);
+            this.btnSend.Location = new System.Drawing.Point(221, 427);
             this.btnSend.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(100, 28);
@@ -55,137 +51,102 @@
             this.btnSend.UseVisualStyleBackColor = false;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
-            // btnAudio
-            // 
-            this.btnAudio.BackColor = System.Drawing.Color.AliceBlue;
-            this.btnAudio.Location = new System.Drawing.Point(0, 17);
-            this.btnAudio.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnAudio.Name = "btnAudio";
-            this.btnAudio.Size = new System.Drawing.Size(150, 28);
-            this.btnAudio.TabIndex = 4;
-            this.btnAudio.Text = "RECORD AUDIO";
-            this.btnAudio.UseVisualStyleBackColor = false;
-            // 
-            // btnClear
-            // 
-            this.btnClear.BackColor = System.Drawing.Color.RosyBrown;
-            this.btnClear.ForeColor = System.Drawing.Color.White;
-            this.btnClear.Location = new System.Drawing.Point(0, 89);
-            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(75, 28);
-            this.btnClear.TabIndex = 2;
-            this.btnClear.Text = "CLEAR";
-            this.btnClear.UseVisualStyleBackColor = false;
-            // 
-            // btnMessages
-            // 
-            this.btnMessages.BackColor = System.Drawing.Color.AliceBlue;
-            this.btnMessages.Location = new System.Drawing.Point(194, 17);
-            this.btnMessages.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnMessages.Name = "btnMessages";
-            this.btnMessages.Size = new System.Drawing.Size(100, 100);
-            this.btnMessages.TabIndex = 5;
-            this.btnMessages.Text = "MESSAGES";
-            this.btnMessages.UseVisualStyleBackColor = false;
-            this.btnMessages.Click += new System.EventHandler(this.btnMessages_Click);
-            // 
             // cboAddresses
             // 
             this.cboAddresses.FormattingEnabled = true;
-            this.cboAddresses.Location = new System.Drawing.Point(16, -1);
+            this.cboAddresses.Location = new System.Drawing.Point(12, 38);
             this.cboAddresses.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cboAddresses.Name = "cboAddresses";
             this.cboAddresses.Size = new System.Drawing.Size(150, 28);
             this.cboAddresses.TabIndex = 6;
             this.cboAddresses.Text = "To:";
             // 
-            // grpMain
+            // txtOutgoing
             // 
-            this.grpMain.Controls.Add(this.btnVideo);
-            this.grpMain.Controls.Add(this.btnMessages);
-            this.grpMain.Controls.Add(this.btnClear);
-            this.grpMain.Controls.Add(this.btnAudio);
-            this.grpMain.Location = new System.Drawing.Point(16, 465);
-            this.grpMain.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.grpMain.Name = "grpMain";
-            this.grpMain.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.grpMain.Size = new System.Drawing.Size(300, 125);
-            this.grpMain.TabIndex = 6;
-            this.grpMain.TabStop = false;
+            this.txtOutgoing.BackColor = System.Drawing.SystemColors.Window;
+            this.txtOutgoing.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtOutgoing.Enabled = false;
+            this.txtOutgoing.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtOutgoing.Location = new System.Drawing.Point(11, 72);
+            this.txtOutgoing.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtOutgoing.Name = "txtOutgoing";
+            this.txtOutgoing.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtOutgoing.Size = new System.Drawing.Size(310, 350);
+            this.txtOutgoing.TabIndex = 0;
+            this.txtOutgoing.Text = "\"User can type directly to textbox\"";
             // 
-            // btnVideo
+            // cboFileList
             // 
-            this.btnVideo.BackColor = System.Drawing.Color.AliceBlue;
-            this.btnVideo.Location = new System.Drawing.Point(0, 53);
-            this.btnVideo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnVideo.Name = "btnVideo";
-            this.btnVideo.Size = new System.Drawing.Size(150, 28);
-            this.btnVideo.TabIndex = 7;
-            this.btnVideo.Text = "UPLOAD VIDEO";
-            this.btnVideo.UseVisualStyleBackColor = false;
+            this.cboFileList.BackColor = System.Drawing.SystemColors.Window;
+            this.cboFileList.Enabled = false;
+            this.cboFileList.FormattingEnabled = true;
+            this.cboFileList.Location = new System.Drawing.Point(12, 428);
+            this.cboFileList.Name = "cboFileList";
+            this.cboFileList.Size = new System.Drawing.Size(175, 28);
+            this.cboFileList.TabIndex = 7;
+            this.cboFileList.Text = "\"Select existing file\"";
             // 
-            // grpMedia
+            // btnCompose
             // 
-            this.grpMedia.Controls.Add(this.label1);
-            this.grpMedia.Location = new System.Drawing.Point(16, 383);
-            this.grpMedia.Name = "grpMedia";
-            this.grpMedia.Size = new System.Drawing.Size(300, 75);
-            this.grpMedia.TabIndex = 7;
-            this.grpMedia.TabStop = false;
+            this.btnCompose.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCompose.Location = new System.Drawing.Point(291, 35);
+            this.btnCompose.Name = "btnCompose";
+            this.btnCompose.Size = new System.Drawing.Size(30, 30);
+            this.btnCompose.TabIndex = 9;
+            this.btnCompose.UseVisualStyleBackColor = true;
+            this.btnCompose.Click += new System.EventHandler(this.btnCompose_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(74, 41);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(185, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "\"Insert Media Player\"";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "\"Compose->\"";
             // 
-            // txtOutgoing
+            // btnUpload
             // 
-            this.txtOutgoing.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtOutgoing.Location = new System.Drawing.Point(16, 35);
-            this.txtOutgoing.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.txtOutgoing.Name = "txtOutgoing";
-            this.txtOutgoing.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.txtOutgoing.Size = new System.Drawing.Size(300, 350);
-            this.txtOutgoing.TabIndex = 0;
-            this.txtOutgoing.Text = "";
+            this.btnUpload.Enabled = false;
+            this.btnUpload.Location = new System.Drawing.Point(12, 462);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(175, 28);
+            this.btnUpload.TabIndex = 11;
+            this.btnUpload.Text = "\"Browse\"";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
             // 
             // Outgoing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(332, 603);
-            this.Controls.Add(this.grpMedia);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(332, 503);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCompose);
+            this.Controls.Add(this.cboFileList);
             this.Controls.Add(this.cboAddresses);
-            this.Controls.Add(this.grpMain);
             this.Controls.Add(this.txtOutgoing);
             this.Controls.Add(this.btnSend);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Outgoing";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OUTGOING";
-            this.grpMain.ResumeLayout(false);
-            this.grpMedia.ResumeLayout(false);
-            this.grpMedia.PerformLayout();
+            this.Load += new System.EventHandler(this.Outgoing_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private Button btnSend;
-        private Button btnAudio;
-        private Button btnClear;
-        private Button btnMessages;
         private ComboBox cboAddresses;
-        private GroupBox grpMain;
-        private GroupBox grpMedia;
-        private Button btnVideo;
-        private Label label1;
         private RichTextBox txtOutgoing;
+        private ComboBox cboFileList;
+        private Button btnCompose;
+        private Label label1;
+        private Button btnUpload;
     }
 }
--- SE-Semester-Project/SE-Semester-Project/Outgoing.cs
+++ SE-Semester-Project/SE-Semester-Project/Outgoing.cs
@@ -1,29 +1,71 @@
+using System.Collections;
+
 namespace SE_Semester_Project
 {
     public partial class Outgoing : Form
     {
+        private List<String> outgoingFilepaths = new List<String>();
+
         public Outgoing()
         {
             InitializeComponent();
         }
 
+
+        //Run immediately when form loads
+        //***note!*** do not modify class constructor. Use this event handler for initialization contents
+        private void Outgoing_Load(object sender, EventArgs e)
+        {
+            //Add test IP to address combo box for presentation
+            cboAddresses.Items.Add("Coleman");
+            cboAddresses.Items.Add("Mitchell");
+            cboAddresses.Items.Add("Tyler");
+            cboAddresses.Items.Add("Victor");
+
+            //store compose symbol and display on btnCompose
+            int i = 11036;
+            char c = (char)i;
+            btnCompose.Text = c.ToString();
+        }
+
         private void btnSend_Click(object sender, EventArgs e)
         {
+            //Customize message font before sending
+            FontDialog font = new FontDialog();
+            font.ShowDialog();
 
         }
-
-        private void btnMessages_Click(object sender, EventArgs e)
+        private void btnUpload_Click(object sender, EventArgs e)
         {
-            //Hide the current form
-            this.Hide();
+            //Create an OpenFileDialog object which provides file browser functionality
+            OpenFileDialog browser = new OpenFileDialog();
 
-            //Create an a Video form object, define the starting position using current form properties.
-            //The object type will be the identical to the "name property" of the form you want to create.
-            Messages messagesForm = new Messages();
-            messagesForm.StartPosition = this.StartPosition;
+            //Create a DialogueResult object which converts a selection to "ok" or "cancel" depending what is selected
+            //If a file is selected, result == Dialgoue.OK, otherwise result == Dialogue.Cancel
+            DialogResult result = browser.ShowDialog();
 
-            //Call Show() function that belongs to form objects
-            messagesForm.Show();
+            //store entire path in a String
+            String path = browser.FileName;
+
+            //Trim path to only include filename and extension
+            String pathTrimmed = Path.GetFileName(path);
+            
+            //If a file is selected, store the full path into class List<> and add trimmed path to cboFileList
+            if(result == DialogResult.OK)
+            {
+                outgoingFilepaths.Add(path);
+                cboFileList.Items.Add(pathTrimmed);
+            }
         }
+
+        private void btnCompose_Click(object sender, EventArgs e)
+        {
+            //Enable composition controls
+            txtOutgoing.Enabled = true;
+            btnUpload.Enabled = true;
+            cboFileList.Enabled = true;
+
+        }
+
     }
 }
