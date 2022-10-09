bfedc385fb1b34db0780e6bf3a4e3c37aa04fd72
Mitchell DePasse <mitchell.depasse@gmail.com>
2022-10-09 04:32:05 -0500

    Commented UserDatabase.cs

--- MoveBit-Server/MoveBit-Server/UserDatabase.cs
+++ MoveBit-Server/MoveBit-Server/UserDatabase.cs
@@ -6,17 +6,36 @@ using System.Threading.Tasks;
 
 namespace MoveBit_Server
 {
+    /// <summary>
+    /// Class for managing storing and retrieving data concerning users.
+    /// This class is implemented as a singleton
+    /// </summary>
     internal class UserDatabase
     {
+
+        // TODO: Change from singleton to static object in ServerMain, probably better that way
+
+        // Table of users, where the primary key is the uersername
         Dictionary<string, UserAccount> userTable;
+        // Singleton instance of this object
         private static UserDatabase theDataBase = null;
+        // Lock for accessing and modifying the database.
         private Object dbLock = new Object();
+
+        /// <summary>
+        /// Constructor for the database. Currently set to private sinc this class
+        /// is a constructor
+        /// </summary>
         private UserDatabase()
         {
             userTable = new Dictionary<string, UserAccount>();
 
         }
 
+        /// <summary>
+        /// Static function for retrieving the singleton database.
+        /// </summary>
+        /// <returns></returns>
         public static UserDatabase GetTheDatabase()
         {
             if(theDataBase == null)
@@ -24,6 +43,13 @@ namespace MoveBit_Server
             return theDataBase;
         }
 
+        /// <summary>
+        /// Function for inserting a new user into the database if the username is currently
+        /// not in the databse. Function body is locked to avoid data race
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public bool insertUserIfNotTaken(string userName, string password)
         {
             lock (dbLock)
@@ -34,16 +60,38 @@ namespace MoveBit_Server
             }
 
         }
+
+        
+        /// <summary>
+        /// Check if the given userName is already in the database's user table
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
         public bool userExists(string userName)
         {
             return userTable.ContainsKey(userName);
         }
 
+
+        /// <summary>
+        /// Check if a given password matches a specific user's password
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public bool passwordValid(string username, string password)
         {
+            // TODO: This function obviously isn't finished, passwords should be stored in some kind of hash
             return userTable[username].password == password;
         }
 
+
+        /// <summary>
+        /// Add a new user to the database given a username and password
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>true if successful</returns>
         public bool addUser(string username, string password)
         {
             UserAccount user = new UserAccount(username, password);
@@ -64,11 +112,23 @@ namespace MoveBit_Server
                 userTable.Add(entry.Key, new UserAccount(entry.Key, entry.Value));
         }
 
+
+        /// <summary>
+        /// Function for getting a user from the database given the user's name.
+        /// Does not check that the user exists.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
         public UserAccount getUser(string userName)
         {
             return userTable[userName];
         }
 
+
+        /// <summary>
+        /// Returns a list of all the users by username that are connected to the server
+        /// </summary>
+        /// <returns></returns>
         public List<string> getActiveUsers()
         {
             List<string> active = new List<string>();
