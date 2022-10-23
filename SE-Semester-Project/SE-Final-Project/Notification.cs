877b50dfeefbdaaf4e7680eb905b792afd16458c
Mitchell DePasse <mitchell.depasse@gmail.com>
2022-10-23 12:18:10 -0500

    Added missing files

---
+++ SE-Semester-Project/SE-Final-Project/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_Final_Project
+{
+    // Notification for something the user should know about
+    internal class Notification
+    {
+
+        public string notification_message;
+        public Notification(string message)
+        {
+            notification_message = message;
+        }
+    }
+}
