aa469479c061fcd4f858feccd79637f9ba9476e1
tstill2 <tstill2@uco.edu>
2022-12-07 21:58:41 -0600

    Removed start and duration dialog boxes and replayes with table panel display and textboxes inside of Main.cs. One set of start/duration for each message type.

--- SE-Semester-Project/SE-Final-Project/view_controller/Messages.Designer.cs
+++ SE-Semester-Project/SE-Final-Project/view_controller/Messages.Designer.cs
@@ -163,7 +163,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MidnightBlue;
-            this.ClientSize = new System.Drawing.Size(784, 683);
+            this.ClientSize = new System.Drawing.Size(787, 683);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.grpTextCanvas);
             this.Controls.Add(this.playerMessages);
